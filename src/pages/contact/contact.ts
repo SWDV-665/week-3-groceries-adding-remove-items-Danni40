0cd9fb216be46fa73ed9f14a45598cb2df39e9c5
Danni40 <dlewis2@live.maryville.edu>
2018-11-05 18:31:36 -0600

    adding and removing items from the grocery list using Ionic.

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +8,43 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
+  sharePage() {
+    console.log("Share Page");
+    this.showSharePagePrompt();
+  }
 
+  showSharePagePrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Share Page',
+      message: "Please enter details",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'email',
+          placeholder: 'Email'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Send',
+          handler: item => {
+            console.log('Page sent', item);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,98 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home1.html'
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title = "Grocery";
 
+  items = [
+    {
+      name: "Milk",
+      quantity: 1    
+    },
+    {
+      name: "Bread",
+      quantity: 1    
+    },
+    {
+      name: "Eggs",
+      quantity: 18    
+    },
+    {
+      name: "Bananas",
+      quantity: 1    
+    },
+    {
+      name: "Corn",
+      quantity: 1    
+    },
+    {
+      name: "Breakfast Shake",
+      quantity: 1    
+    },
+    {
+      name: "Tea",
+      quantity: 1    
+    },
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+
+  }
+
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index, 1);
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
